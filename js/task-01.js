4682f7bf595fca9d6ceff57f610aaffb7bec8f7e
Volodymyr Proniuk <111579555+vpronyuk@users.noreply.github.com>
2023-01-14 10:29:54 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,13 @@
 const categories = document.getElementById("categories");
-
-// 1. Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
 const categoriesCounter = categories.childElementCount;
-console.log(`Number of categories: ${categoriesCounter}`);
+console.log(`Number of categories: ${categoriesCounter}\n`);
 
-// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>)
-// і кількість елементів в категорії(усіх < li >, вкладених в нього).
 const headersText = document.querySelectorAll(".item");
 for (const headerText of headersText) {
-  console.log(headerText.firstElementChild.textContent);
-  console.log(headerText.children.textContent);
+  console.log(`Category: ${headerText.firstElementChild.textContent}`);
+
+  const subLiElem = headerText.querySelectorAll("ul");
+  for (const liElem of subLiElem) {
+    console.log(`Elements: ${liElem.childElementCount}\n`);
+  }
 }
-console.log(headersText);
